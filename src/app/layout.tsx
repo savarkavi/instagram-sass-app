d4244d761e07e1f18b713bef06e43343aa633863
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-05-13 07:14:06 +0530

    style: update body class to include font-serif for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${geistSans.variable} ${geistMono.variable} font-serif antialiased`}
         >
           <ThemeProvider
             attribute="class"
