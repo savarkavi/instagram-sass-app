e9bfefab6a701476a4dc98f79e3f436f6875b72c
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-05-18 14:15:42 +0530

    feat: add usePath hook for improved route management

---
+++ src/hooks/use-path.ts
@@ -0,0 +1,17 @@
+import { usePathname } from "next/navigation";
+
+export const usePath = () => {
+  const pathname = usePathname();
+
+  const pathSplit = pathname.split("/");
+
+  let currentRoute;
+
+  if (pathSplit.length === 3) {
+    currentRoute = "home";
+  } else {
+    currentRoute = pathSplit[pathSplit.length - 1];
+  }
+
+  return { currentRoute };
+};
